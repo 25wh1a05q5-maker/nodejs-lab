0ef44668fdd92146e90718dd2d3c431264d99d40
25wh1a05q5-maker <25wh1a05q5@bvrithyderabad.edu.in>
2026-09-23 15:46:01 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+    res.write("Hello welcome to Node.js");
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log("Server is running at http://localhost:3000");
+});
